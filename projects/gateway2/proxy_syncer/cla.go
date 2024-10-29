e24e490356158583417b87c735f7e1f3c344e985
Steven Landow <steven@landow.dev>
2024-10-29 11:29:23 -0700

    extensibility in krt

--- projects/gateway2/proxy_syncer/cla.go
+++ projects/gateway2/proxy_syncer/cla.go
@@ -30,26 +30,25 @@ func (c EndpointResources) Equals(in EndpointResources) bool {
 
 // TODO: this is needed temporary while we don't have the per-upstream translation done.
 // once the plugins are fixed to support it, we can have the proxy translation skip upstreams/endpoints and remove this collection
-func newEnvoyEndpoints(glooEndpoints krt.Collection[EndpointsForUpstream]) krt.Collection[EndpointResources] {
-
-	clas := krt.NewCollection(glooEndpoints, func(_ krt.HandlerContext, ep EndpointsForUpstream) *EndpointResources {
+func newEnvoyEndpoints(glooEndpoints krt.Collection[krtcollections.EndpointsForUpstream]) krt.Collection[EndpointResources] {
+	clas := krt.NewCollection(glooEndpoints, func(_ krt.HandlerContext, ep krtcollections.EndpointsForUpstream) *EndpointResources {
 		return TransformEndpointToResources(ep)
 	})
 	return clas
 }
 
-func TransformEndpointToResources(ep EndpointsForUpstream) *EndpointResources {
+func TransformEndpointToResources(ep krtcollections.EndpointsForUpstream) *EndpointResources {
 	cla := prioritize(ep)
 	return &EndpointResources{
 		Endpoints:        resource.NewEnvoyResource(cla),
-		EndpointsVersion: ep.lbEpsEqualityHash,
+		EndpointsVersion: ep.LbEpsEqualityHash,
 		UpstreamRef:      ep.UpstreamRef,
 	}
 }
 
-func prioritize(ep EndpointsForUpstream) *envoy_config_endpoint_v3.ClusterLoadAssignment {
+func prioritize(ep krtcollections.EndpointsForUpstream) *envoy_config_endpoint_v3.ClusterLoadAssignment {
 	cla := &envoy_config_endpoint_v3.ClusterLoadAssignment{
-		ClusterName: ep.clusterName,
+		ClusterName: ep.ClusterName,
 	}
 	for loc, eps := range ep.LbEps {
 		var l *envoy_config_core_v3.Locality
@@ -105,10 +104,10 @@ func (ie *PerClientEnvoyEndpoints) FetchEndpointsForClient(kctx krt.HandlerConte
 }
 
 func NewPerClientEnvoyEndpoints(logger *zap.Logger, uccs krt.Collection[krtcollections.UniqlyConnectedClient],
-	glooEndpoints krt.Collection[EndpointsForUpstream],
-	destinationRulesIndex DestinationRuleIndex) PerClientEnvoyEndpoints {
-
-	clas := krt.NewManyCollection(glooEndpoints, func(kctx krt.HandlerContext, ep EndpointsForUpstream) []UccWithEndpoints {
+	glooEndpoints krt.Collection[krtcollections.EndpointsForUpstream],
+	destinationRulesIndex DestinationRuleIndex,
+) PerClientEnvoyEndpoints {
+	clas := krt.NewManyCollection(glooEndpoints, func(kctx krt.HandlerContext, ep krtcollections.EndpointsForUpstream) []UccWithEndpoints {
 		uccs := krt.Fetch(kctx, uccs)
 		uccWithEndpointsRet := make([]UccWithEndpoints, 0, len(uccs))
 		for _, ucc := range uccs {
@@ -128,7 +127,7 @@ func NewPerClientEnvoyEndpoints(logger *zap.Logger, uccs krt.Collection[krtcolle
 	}
 }
 
-func PrioritizeEndpoints(logger *zap.Logger, destrule *DestinationRuleWrapper, ep EndpointsForUpstream, ucc krtcollections.UniqlyConnectedClient) UccWithEndpoints {
+func PrioritizeEndpoints(logger *zap.Logger, destrule *DestinationRuleWrapper, ep krtcollections.EndpointsForUpstream, ucc krtcollections.UniqlyConnectedClient) UccWithEndpoints {
 	var additionalHash uint64
 	var priorityInfo *PriorityInfo
 	if destrule != nil {
@@ -148,7 +147,7 @@ func PrioritizeEndpoints(logger *zap.Logger, destrule *DestinationRuleWrapper, e
 	return UccWithEndpoints{
 		Client:        ucc,
 		Endpoints:     resource.NewEnvoyResource(cla),
-		EndpointsHash: ep.lbEpsEqualityHash ^ additionalHash,
+		EndpointsHash: ep.LbEpsEqualityHash ^ additionalHash,
 		endpointsName: ep.ResourceName(),
 	}
 }
--- projects/gateway2/proxy_syncer/endpoints_prioritize.go
+++ projects/gateway2/proxy_syncer/endpoints_prioritize.go
@@ -80,9 +80,9 @@ func priorityLabelOverrides(labels []string) ([]string, map[string]string) {
 	return priorityLabels, overriddenValueByLabel
 }
 
-func prioritizeWithLbInfo(logger *zap.Logger, ep EndpointsForUpstream, lbInfo LoadBalancingInfo) *envoy_config_endpoint_v3.ClusterLoadAssignment {
+func prioritizeWithLbInfo(logger *zap.Logger, ep krtcollections.EndpointsForUpstream, lbInfo LoadBalancingInfo) *envoy_config_endpoint_v3.ClusterLoadAssignment {
 	cla := &envoy_config_endpoint_v3.ClusterLoadAssignment{
-		ClusterName: ep.clusterName,
+		ClusterName: ep.ClusterName,
 	}
 	totalEndpoints := 0
 	for loc, eps := range ep.LbEps {
@@ -125,17 +125,18 @@ func prioritizeWithLbInfo(logger *zap.Logger, ep EndpointsForUpstream, lbInfo Lo
 	return cla
 }
 
-func getEndpoints(eps []EndpointWithMd, lbinfo LoadBalancingInfo) []*envoy_config_endpoint_v3.LocalityLbEndpoints {
+func getEndpoints(eps []krtcollections.EndpointWithMd, lbinfo LoadBalancingInfo) []*envoy_config_endpoint_v3.LocalityLbEndpoints {
 	if lbinfo.PriorityInfo != nil && lbinfo.PriorityInfo.FailoverPriority != nil {
 		return applyFailoverPriorityPerLocality(eps, lbinfo)
 	}
 	return []*envoy_config_endpoint_v3.LocalityLbEndpoints{{
-		LbEndpoints: slices.Map(eps, func(e EndpointWithMd) *envoy_config_endpoint_v3.LbEndpoint { return e.LbEndpoint }),
+		LbEndpoints: slices.Map(eps, func(e krtcollections.EndpointWithMd) *envoy_config_endpoint_v3.LbEndpoint { return e.LbEndpoint }),
 	}}
 }
 
 func applyFailoverPriorityPerLocality(
-	eps []EndpointWithMd, lbinfo LoadBalancingInfo) []*envoy_config_endpoint_v3.LocalityLbEndpoints {
+	eps []krtcollections.EndpointWithMd, lbinfo LoadBalancingInfo,
+) []*envoy_config_endpoint_v3.LocalityLbEndpoints {
 	// key is priority, value is the index of LocalityLbEndpoints.LbEndpoints
 	priorityMap := map[int][]int{}
 	for i, ep := range eps {
@@ -228,6 +229,7 @@ func applyLocalityFailover(
 		}
 	}
 }
+
 func LbPriority(proxyLocality, endpointsLocality *envoy_config_core_v3.Locality) int {
 	if proxyLocality.GetRegion() == endpointsLocality.GetRegion() {
 		if proxyLocality.GetZone() == endpointsLocality.GetZone() {
